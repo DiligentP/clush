caf920c1b4a380ac686358068f036424be43005b
diligentp <qkrwjdgus0603@gmail.com>
2025-02-17 07:54:44 +0900

    feat: Swagger 서버 URL 및 Vite allowedHosts 설정 추가
    - SwaggerConfig에 서버 URL(clush-be.diligentp.com, localhost) 추가
    - Vite 설정에 allowedHosts(clush.diligentp.com, localhost) 추가

--- backend/src/main/java/com/clush/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/clush/backend/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.clush.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,8 @@ public class SwaggerConfig {
                         .title("Clush API 문서")
                         .version("1.0.0")
                         .description("Clush 프로젝트 API 명세서")
-                );
+                )
+                .addServersItem(new Server().url("https://clush-be.diligentp.com"))
+                .addServersItem(new Server().url("http://localhost:8080"));
     }
 } 
